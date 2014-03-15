03692e9d543c05e3675f9893380aea5357ef2e27
Chris Inskip <chrisinskip94@gmail.com>
2014-03-15 01:11:27 +0000

    Added the ability to change the stroke width.... (and adjust the padding)

--- AntGame/src/GUI_Testing.java
+++ AntGame/src/GUI_Testing.java
@@ -3,27 +3,37 @@ import javax.swing.*;
 
 public class GUI_Testing{
 	public static void main(String args[]) {
-		int rows = 22;
-		int cols = 14;
-		int size = 16;
-		HexGrid grid = new HexGrid(rows, cols, size);
+		int rows = 40;
+		int cols = 30;
+		int size = 8;
+		int strokeWidth = 1;
+		HexGrid grid = new HexGrid(rows, cols, size, strokeWidth);
 		
 		JFrame window = new JFrame();
 		window.setSize(640, 420);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(grid);
 		window.setVisible(true);
+		
+		
+		//EDIT THE HEX GRID, AFTER CREATION.
+		grid.getHexagon(0, 0).setFillColor(Hexagon.BLACK_ANT_COLOR);
+		grid.getHexagon(3, 6).setFillColor(Hexagon.RED_ANT_COLOR);
+		grid.getHexagon(8, 7).setFillColor(Hexagon.BLACK_ANT_COLOR);
+		grid.getHexagon(10, 1).setFillColor(Hexagon.BLACK_ANT_COLOR);
+		grid.getHexagon(8, 7).setFillColor(Hexagon.EMPTY_CELL_COLOR);
+
 	}
 }
 
 class HexGrid extends JPanel {
 	Hexagon[][] grid;
 	
-	public HexGrid(int rows, int cols, int size) {
+	public HexGrid(int rows, int cols, int size, int strokeWidth) {
 		grid = new Hexagon[rows][cols];
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				grid[row][col] = new Hexagon(row, col, size);
+				grid[row][col] = new Hexagon(row, col, size, strokeWidth);
 			}
 		}
 	}
@@ -48,33 +58,44 @@ class HexGrid extends JPanel {
 		g2.fillPolygon(h);
 		
 		// Draw the outline.
-		g2.setStroke(new BasicStroke(1));
+		g2.setStroke(new BasicStroke(h.getStrokeWidth()));
 		g2.setColor(h.getOutlineColor());
 		g2.drawPolygon(h);
+	}
+	
+	public void setHexagon(int x, int y, Hexagon h) {
+		grid[x][y] = h;
+	}
+	
+	public Hexagon getHexagon(int x, int y) {
+		return grid[x][y];
 	}
 }
 
 
 class Hexagon extends Polygon {
-	public static final Color redAntColor = Color.RED; 
-	public static final Color blackAntColor = Color.BLACK;
-	
-	private Color fill;
-	private Color outline;
+	public static final Color RED_ANT_COLOR = Color.RED; 
+	public static final Color BLACK_ANT_COLOR = Color.BLACK;
+	public static final Color EMPTY_CELL_COLOR = Color.WHITE;
+
+	private Color fillColor;
+	private Color outlineColor;
+	private int strokeWidth;
 	private int centerX; // Centre x co-ord (px)
 	private int centerY; // Centre y co-ord (px)
 	private final int SIZE; // Size of hexagon (similar to the radius)
 	
-	public Hexagon(int x, int y, int size) {
+	public Hexagon(int x, int y, int size, int strokeWidth) {
 		this.SIZE = size;
-		this.fill = Color.WHITE;
-		this.outline = Color.BLACK;
+		this.fillColor = Color.WHITE;
+		this.outlineColor = Color.BLACK;
+		this.strokeWidth = strokeWidth;
 		
 		int xOffset = 0; // Used to interleave hexagons 
 		int yOffset = 0; // Used to interleave hexagons
 		
-		int height = SIZE * 2;
-		int width = (int) (Math.sqrt(3)/2 * height); 
+		int height = SIZE * 2 + strokeWidth/2;
+		int width = (int) (Math.sqrt(3)/2 * height)+ strokeWidth/2; 
 		
 		// If odd row, shift right.
 		if (y % 2 != 0 && y != 0) {
@@ -96,19 +117,23 @@ class Hexagon extends Polygon {
 	    }
 	}
 	
+	public int getStrokeWidth() {
+		return this.strokeWidth;
+	}
+
 	public Color getFillColor() {
-		return this.fill;
+		return this.fillColor;
 	}
 	
 	public Color getOutlineColor() {
-		return this.outline;
+		return this.outlineColor;
 	}
 	
 	public void setFillColor(Color fill) {
-		this.fill = fill;
+		this.fillColor = fill;
 	}
 	
 	public void setOutlineColor(Color outline) {
-		this.outline = outline;
+		this.outlineColor = outline;
 	}
 }
